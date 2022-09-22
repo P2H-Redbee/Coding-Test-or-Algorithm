8de8c78d6fdef185d6657096b8390a28053375cb
P2H-Redbee <rbride7@gmail.com>
2022-09-22 23:19:15 +0900

    p.201 문제 풀이 2개

---
+++ 이것이 코딩테스트다/p.201 탐색 실전 7-2 떡볶이 떡 만들기1.py
@@ -0,0 +1,21 @@
+# p.201
+# 탐색 실전 7-2 떡볶이 떡 만들기
+
+n, m = map(int, input().split())
+
+dduk = list(map(int, input().split()))
+dduk.sort() # 정렬 되어 있어야 정확한 최대길이를 찾을 수 있다.
+
+cut = max(dduk) - 1 # 칼 길이 설정
+while cut > 0 :
+    count = 0
+    for i in dduk :
+        if i - cut > 0 : # 떡을 칼로 자를 수 있는 정도의 길이라면 자른 길이 계산. 떡길이 - 칼길이
+            count += i - cut
+        else :
+            continue
+    if count >= m : 
+        print(cut)
+        break
+    else :
+        cut -= 1
---
+++ 이것이 코딩테스트다/p.201 탐색 실전 7-2 떡볶이 떡 만들기2.py
@@ -0,0 +1,25 @@
+# p.201
+# 실전7-2 떡볶이 떡 만들기2
+
+n, m = map(int, input().split())
+
+dduk = list(map(int, input().split()))
+
+i = 0
+j = max(dduk)
+
+while i<=j :
+    mid = (i + j) //2
+    length = 0
+
+    #길이 계산하기
+    for x in dduk :
+        if x > mid :
+            length += x - mid
+    if length >= m :   
+        i = mid + 1
+        top = mid
+    elif length < m :
+        j = mid - 1
+
+print(top)
